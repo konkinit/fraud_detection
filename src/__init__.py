02a7ae742de1d98af24c9109a583b882d6356399
konkinit <ikonkobo.idrissa@gmail.com>
2023-07-22 13:32:45 +0000

    utils functions

--- src/__init__.py
+++ src/__init__.py
@@ -1,10 +1,11 @@
 from .conf import configs
 from .models import autoencoder
-from .utils import data_transform
+from .utils import data_transform, ml_partitions_indices
 
 
 __all__ = [
     "autoencoder",
     "configs",
-    "data_transform"
+    "data_transform",
+    "ml_partitions_indices"
 ]
--- src/utils.py
+++ src/utils.py
@@ -1,2 +1,20 @@
+from numpy import array, append, array_split
+from typing import List
+
+
 def data_transform():
     pass
+
+
+def ml_partitions_indices(
+    n: int,
+    split_fractions: List[float]
+) -> List[List[int]]:
+    split_fractions = array(split_fractions)
+    assert split_fractions.sum().round(1) == 1.0
+    split_freq = append(
+        t_v_freq := (n*split_fractions[:2]).astype(int),
+        n-t_v_freq.sum()
+    )
+    assert n == split_freq.sum()
+    return split_freq
